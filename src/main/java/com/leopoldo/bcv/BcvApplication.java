e24efd487aeb308aa2d424d7b8b25937af0882ef
burgoslp <pinedo.burgoslp3@gmail.com>
2025-11-05 16:12:04 -0400

    estructura inicial del proyecto

---
+++ src/main/java/com/leopoldo/bcv/BcvApplication.java
@@ -0,0 +1,13 @@
+package com.leopoldo.bcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leopoldo/bcv/BcvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leopoldo.bcv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
